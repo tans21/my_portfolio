b946fe73aa2b49c67a954ffb27e167de0a98bd72
Anshika Chauhan <anshikachauhan212002@gmail.com>
2025-08-16 12:50:49 +0530

    Footer completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
 
 export const Home = () => {
@@ -26,6 +27,7 @@ export const Home = () => {
             <ContactSection />
         </main>
         {/* Footer */}
+            <Footer />
 
     </div>
     );
